b4b4d100529abe7641839903c70b9dddc6ddca4d
FRANCISCO CARLOS PIERNA <pierna_f@hotmail.com>
2023-05-23 14:36:45 -0300

    python testefuncoesrecursivaspython

--- src/novo.py
+++ src/novo.py
@@ -1,5 +1,17 @@
 def soma(x: float, y: float) -> float:
-    return x + y 
+    return x + y
+ 
 
 if __name__ == "__main__":
    print(soma(10, 10))
+
+
+
+def fatorial(n):
+  if n == 0 or n == 1:
+    return 1;
+  else:
+    return n * fatorial(n - 1)
+
+
+print(fatorial(5))
--- testes/testesoma.py
+++ testes/testesoma.py
@@ -1,6 +1,5 @@
 import unittest
-from src.novo import soma
-
+from src.novo import soma, fatorial
 
 class testsoma(unittest.TestCase):
     def test_retorno_soma(self):
@@ -8,3 +7,10 @@ class testsoma(unittest.TestCase):
 
     def teste_retorno_soma_10_20(self):
         self.assertEqual(soma(10, 20), 30)
+    
+    def test_fatorial(self):
+        self.assertEqual(fatorial(5), 120)       
+
+    def test_fatorial1(self):
+        self.assertEqual(fatorial(3), 6)      
+
